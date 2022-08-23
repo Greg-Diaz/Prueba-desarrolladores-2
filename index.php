257c33c47d28099e67ace209870e8e47c493aa15
Greg-Diaz <61952683+Greg-Diaz@users.noreply.github.com>
2022-08-23 09:35:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,21 +38,13 @@ $rangos = new lrangos ();
 
 $lista= $rangos->getrangos();
 
-foreach ($lista as $ran)
-
-
-    {
-
-        echo $ran->cal_rango($ran->get_nui(),$ran->get_nf()) ;
-         echo '<br>';
-    }
 
 $nui = $_POST['rangoi'];
    $rf = $_POST['rangof'];
 
     $r = new rango($nui,$rf);
 
-    echo $r->cal_rango($r->get_nui(),$r->get_nf()) ;
+    echo $r->cal_rango($_POST['rangoi'],$_POST['rangof']) ;
     echo "<hr>"
 
     

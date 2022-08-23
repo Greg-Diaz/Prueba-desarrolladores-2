1644bef9b7fb482a33b1843357eafa187779ae51
Greg-Diaz <61952683+Greg-Diaz@users.noreply.github.com>
2022-08-23 09:58:28 -0500

    Update action.php

--- action.php
+++ action.php
@@ -8,12 +8,13 @@ $rangos = new lrangos ();
 $lista= $rangos->getrangos();
 
 
-  $nui = $_POST['rangoi'] ;
-   $rf = $_POST['rangof'];
+    $nui = $_POST['rangoi'] ;
+    $rf = $_POST['rangof'];
+    
 
     $r = new rango($nui,$rf);
 
-    echo $r->get_nui();
+  
     
     echo $r->cal_rango($r->get_nui(),$r->get_nf()) ;
     echo "<hr>"

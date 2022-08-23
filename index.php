aa21f726b18bbe4e4120aedeb33b0cc20c7e7d7c
Greg-Diaz <61952683+Greg-Diaz@users.noreply.github.com>
2022-08-23 09:53:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,30 +28,7 @@
 </div>
 
 
-  
-<?php
 
-require_once ("crango.php");
-
-
-$rangos = new lrangos ();
-
-$lista= $rangos->getrangos();
-
-
-  $nui = $_POST['rangoi'] ;
-   $rf = $_POST['rangof'];
-
-    $r = new rango($nui,$rf);
-
-    echo $r->get_nui();
-    
-    echo $r->cal_rango($r->get_nui(),$r->get_nf()) ;
-    echo "<hr>"
-
-    
-    
-?>
 
 <H5>Jose Gregorio Diaz Torres</H5>
 </div>

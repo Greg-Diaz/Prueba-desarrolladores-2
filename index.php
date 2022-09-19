15863d9215c266519113b2ffd237362a84bc635a
Greg-Diaz <61952683+Greg-Diaz@users.noreply.github.com>
2022-09-19 08:43:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,35 +26,7 @@
 <div class="mb-3">
 <button type="submit" class="btn btn-primary mb-3">Buscar numeros perfectos</button>
 </div>
-<?php
-require_once ("crango.php");
-
-
-$rangos = new lrangos ();
-
-$lista= $rangos->getrangos();
-
-foreach ($lista as $ran)
-
-
-    {
-
-        echo $ran->cal_rango($ran->get_nui(),$ran->get_nf()) ;
-         echo '<br>';
-    }
-
-
-    $nui = $_POST['rangoi'] ;
-    $rf = $_POST['rangof'];
-    
-
-    $r = new rango($nui,$rf);
-
-  
-    
-    echo $r->cal_rango($r->get_nui(),$r->get_nf()) ;
-    echo "<hr>"
-        
+       
 
 
 <H5>Jose Gregorio Diaz Torres</H5>
